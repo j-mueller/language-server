d47fb16bbe3cdf7894de536cea6f1daa92c29048
Jann Mueller <j.mueller.11@ucl.ac.uk>
2017-03-09 16:10:55 +0000

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import LanguageServer
+
+main :: IO ()
+main = putStrLn "language-server example (TBD)"
---
+++ src/LanguageServer.hs
@@ -0,0 +1,6 @@
+module LanguageServer
+    ( someFunc
+    ) where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
