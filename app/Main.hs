9219d6c95f3f5ca31400284bf4cca6beeaa6de76
Jann Mueller <j.mueller.11@ucl.ac.uk>
2017-03-11 21:47:06 +0000

    Use stdio in example

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.Semigroup
-import Network.Socket hiding (recv)
-import Network.Socket.ByteString.Lazy
+import           Control.Monad (forM_, when)
+import qualified Data.ByteString.Lazy.Char8 as B
+import           Data.Maybe (fromMaybe)
+import           System.IO (BufferMode (LineBuffering), hSetBuffering, stdout)
 
-import Protocols.LanguageServer.V3 (run)
+import           Protocols.LanguageServer.V3 (run)
 
 main :: IO ()
 main = do
-  sock <- socket AF_INET Stream 0
-  setSocketOption sock ReuseAddr 1
-  bind sock (SockAddrInet 4242 iNADDR_ANY)
-  listen sock 1
-  (conn, _) <- accept sock
-  mainLoop conn
-  close conn
-  close sock
-
-mainLoop :: Socket -> IO ()
-mainLoop conn = do
-  msg <- recv conn 1024
-  response <- run msg
-  sendAll conn (maybe mempty id response) >> mainLoop conn
+  hSetBuffering stdout LineBuffering
+  contents <- B.getContents
+  forM_ (B.lines contents) $ \request -> do
+    run request >>= B.putStrLn . fromMaybe ""
